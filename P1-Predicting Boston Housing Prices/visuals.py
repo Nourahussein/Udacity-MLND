cb639ba8c6ed49e77898f021266bb0ec68a6411d
Nourahussein <nourahussein193@gmail.com>
2017-12-24 14:46:27 +0200

    update visulas

--- P1-Predicting Boston Housing Prices/visuals.py
+++ P1-Predicting Boston Housing Prices/visuals.py
@@ -9,147 +9,122 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 ###########################################
 
+import matplotlib.pyplot as pl
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import sklearn.learning_curve as curves
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.cross_validation import ShuffleSplit, train_test_split
 
-def filter_data(data, condition):
-    """
-    Remove elements that do not match the condition provided.
-    Takes a data list as input and returns a filtered list.
-    Conditions should be a list of strings of the following format:
-      '<field> <op> <value>'
-    where the following operations are valid: >, <, >=, <=, ==, !=
+def ModelLearning(X, y):
+    """ Calculates the performance of several models with varying sizes of training data.
+        The learning and testing scores for each model are then plotted. """
     
-    Example: ["Sex == 'male'", 'Age < 18']
-    """
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
 
-    field, op, value = condition.split(" ")
-    
-    # convert value into number or strip excess quotes if string
-    try:
-        value = float(value)
-    except:
-        value = value.strip("\'\"")
-    
-    # get booleans for filtering
-    if op == ">":
-        matches = data[field] > value
-    elif op == "<":
-        matches = data[field] < value
-    elif op == ">=":
-        matches = data[field] >= value
-    elif op == "<=":
-        matches = data[field] <= value
-    elif op == "==":
-        matches = data[field] == value
-    elif op == "!=":
-        matches = data[field] != value
-    else: # catch invalid operation codes
-        raise Exception("Invalid comparison operator. Only >, <, >=, <=, ==, != allowed.")
-    
-    # filter data and outcomes
-    data = data[matches].reset_index(drop = True)
-    return data
+    # Generate the training set sizes increasing by 50
+    train_sizes = np.rint(np.linspace(1, X.shape[0]*0.8 - 1, 9)).astype(int)
 
-def survival_stats(data, outcomes, key, filters = []):
-    """
-    Print out selected statistics regarding survival, given a feature of
-    interest and any number of filters (including no filters)
-    """
-    
-    # Check that the key exists
-    if key not in data.columns.values :
-        print ("'{}' is not a feature of the Titanic data. Did you spell something wrong?".format(key))
-        return False
+    # Create the figure window
+    fig = pl.figure(figsize=(10,7))
 
-    # Return the function before visualizing if 'Cabin' or 'Ticket'
-    # is selected: too many unique categories to display
-    if(key == 'Cabin' or key == 'PassengerId' or key == 'Ticket'):
-        print ("'{}' has too many unique categories to display! Try a different feature.".format(key))
-        return False
-
-    # Merge data and outcomes into single dataframe
-    all_data = pd.concat([data, outcomes.to_frame()], axis = 1)
-    
-    # Apply filters to data
-    for condition in filters:
-        all_data = filter_data(all_data, condition)
-
-    # Create outcomes DataFrame
-    all_data = all_data[[key, 'Survived']]
-    
-    # Create plotting figure
-    plt.figure(figsize=(8,6))
-
-    # 'Numerical' features
-    if(key == 'Age' or key == 'Fare'):
+    # Create three different models based on max_depth
+    for k, depth in enumerate([1,3,6,10]):
         
-        # Remove NaN values from Age data
-        all_data = all_data[~np.isnan(all_data[key])]
+        # Create a Decision tree regressor at max_depth = depth
+        regressor = DecisionTreeRegressor(max_depth = depth)
+
+        # Calculate the training and testing scores
+        sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
+            cv = cv, train_sizes = train_sizes, scoring = 'r2')
         
-        # Divide the range of data into bins and count survival rates
-        min_value = all_data[key].min()
-        max_value = all_data[key].max()
-        value_range = max_value - min_value
+        # Find the mean and standard deviation for smoothing
+        train_std = np.std(train_scores, axis = 1)
+        train_mean = np.mean(train_scores, axis = 1)
+        test_std = np.std(test_scores, axis = 1)
+        test_mean = np.mean(test_scores, axis = 1)
 
-        # 'Fares' has larger range of values than 'Age' so create more bins
-        if(key == 'Fare'):
-            bins = np.arange(0, all_data['Fare'].max() + 20, 20)
-        if(key == 'Age'):
-            bins = np.arange(0, all_data['Age'].max() + 10, 10)
+        # Subplot the learning curve 
+        ax = fig.add_subplot(2, 2, k+1)
+        ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
+        ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
+        ax.fill_between(sizes, train_mean - train_std, \
+            train_mean + train_std, alpha = 0.15, color = 'r')
+        ax.fill_between(sizes, test_mean - test_std, \
+            test_mean + test_std, alpha = 0.15, color = 'g')
         
-        # Overlay each bin's survival rates
-        nonsurv_vals = all_data[all_data['Survived'] == 0][key].reset_index(drop = True)
-        surv_vals = all_data[all_data['Survived'] == 1][key].reset_index(drop = True)
-        plt.hist(nonsurv_vals, bins = bins, alpha = 0.6,
-                 color = 'red', label = 'Did not survive')
-        plt.hist(surv_vals, bins = bins, alpha = 0.6,
-                 color = 'green', label = 'Survived')
+        # Labels
+        ax.set_title('max_depth = %s'%(depth))
+        ax.set_xlabel('Number of Training Points')
+        ax.set_ylabel('Score')
+        ax.set_xlim([0, X.shape[0]*0.8])
+        ax.set_ylim([-0.05, 1.05])
     
-        # Add legend to plot
-        plt.xlim(0, bins.max())
-        plt.legend(framealpha = 0.8)
+    # Visual aesthetics
+    ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
+    fig.suptitle('Decision Tree Regressor Learning Performances', fontsize = 16, y = 1.03)
+    fig.tight_layout()
+    fig.show()
+
+
+def ModelComplexity(X, y):
+    """ Calculates the performance of the model as model complexity increases.
+        The learning and testing errors rates are then plotted. """
     
-    # 'Categorical' features
-    else:
-       
-        # Set the various categories
-        if(key == 'Pclass'):
-            values = np.arange(1,4)
-        if(key == 'Parch' or key == 'SibSp'):
-            values = np.arange(0,np.max(data[key]) + 1)
-        if(key == 'Embarked'):
-            values = ['C', 'Q', 'S']
-        if(key == 'Sex'):
-            values = ['male', 'female']
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
 
-        # Create DataFrame containing categories and count of each
-        frame = pd.DataFrame(index = np.arange(len(values)), columns=(key,'Survived','NSurvived'))
-        for i, value in enumerate(values):
-            frame.loc[i] = [value, \
-                   len(all_data[(all_data['Survived'] == 1) & (all_data[key] == value)]), \
-                   len(all_data[(all_data['Survived'] == 0) & (all_data[key] == value)])]
+    # Vary the max_depth parameter from 1 to 10
+    max_depth = np.arange(1,11)
 
-        # Set the width of each bar
-        bar_width = 0.4
+    # Calculate the training and testing scores
+    train_scores, test_scores = curves.validation_curve(DecisionTreeRegressor(), X, y, \
+        param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
 
-        # Display each category's survival rates
-        for i in np.arange(len(frame)):
-            nonsurv_bar = plt.bar(i-bar_width, frame.loc[i]['NSurvived'], width = bar_width, color = 'r')
-            surv_bar = plt.bar(i, frame.loc[i]['Survived'], width = bar_width, color = 'g')
+    # Find the mean and standard deviation for smoothing
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
 
-            plt.xticks(np.arange(len(frame)), values)
-            plt.legend((nonsurv_bar[0], surv_bar[0]),('Did not survive', 'Survived'), framealpha = 0.8)
+    # Plot the validation curve
+    pl.figure(figsize=(7, 5))
+    pl.title('Decision Tree Regressor Complexity Performance')
+    pl.plot(max_depth, train_mean, 'o-', color = 'r', label = 'Training Score')
+    pl.plot(max_depth, test_mean, 'o-', color = 'g', label = 'Validation Score')
+    pl.fill_between(max_depth, train_mean - train_std, \
+        train_mean + train_std, alpha = 0.15, color = 'r')
+    pl.fill_between(max_depth, test_mean - test_std, \
+        test_mean + test_std, alpha = 0.15, color = 'g')
+    
+    # Visual aesthetics
+    pl.legend(loc = 'lower right')
+    pl.xlabel('Maximum Depth')
+    pl.ylabel('Score')
+    pl.ylim([-0.05,1.05])
+    pl.show()
 
-    # Common attributes for plot formatting
-    plt.xlabel(key)
-    plt.ylabel('Number of Passengers')
-    plt.title('Passenger Survival Statistics With \'%s\' Feature'%(key))
-    plt.show()
 
-    # Report number of passengers with missing values
-    if sum(pd.isnull(all_data[key])):
-        nan_outcomes = all_data[pd.isnull(all_data[key])]['Survived']
-        print ("Passengers with missing '{}' values: {} ({} survived, {} did not survive)".format( \
-              key, len(nan_outcomes), sum(nan_outcomes == 1), sum(nan_outcomes == 0)))
+def PredictTrials(X, y, fitter, data):
+    """ Performs trials of fitting and predicting data. """
+
+    # Store the predicted prices
+    prices = []
+
+    for k in range(10):
+        # Split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, \
+            test_size = 0.2, random_state = k)
+        
+        # Fit the data
+        reg = fitter(X_train, y_train)
+        
+        # Make a prediction
+        pred = reg.predict([data[0]])[0]
+        prices.append(pred)
+        
+        # Result
+        print ("Trial {}: ${:,.2f}".format(k+1, pred))
+
+    # Display price range
+    print ("\nRange in prices: ${:,.2f}".format(max(prices) - min(prices)))
